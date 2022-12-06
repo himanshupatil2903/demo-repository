d62e1447fa04899c3416bf369e27bac38dba3cae
himanshupatil2903 <119918072+himanshupatil2903@users.noreply.github.com>
2022-12-07 00:24:55 +0530

    first commit

---
+++ DemoProject/src/demoPackage/FirstProgram.java
@@ -0,0 +1,10 @@
+package demoPackage;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
